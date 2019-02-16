32a549085133abcc1ea6efb7009650d8a5de379d
Dakota Clark <bannaray95@gmail.com>
2019-02-16 06:39:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(arg = "")
+def can_be_created_in_a_block(arg = "1")
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
